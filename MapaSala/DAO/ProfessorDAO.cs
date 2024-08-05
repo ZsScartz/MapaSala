571657f5cdfd4c4c4b4d18b59851ce92d3e9de26
ZsScartz <raphaelramos2006@gmail.com>
2024-08-05 13:40:00 -0300

    Adicionando banco de dados

---
+++ MapaSala/DAO/ProfessorDAO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Model.Entidades;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaSala.DAO
+{
+    class ProfessorDAO
+    {
+        private string LinhaConexao = "Server=LS05MPF;Database=AULA_DS;User Id=sa;Password=admsasql;";
+        private SqlConnection Conexao;
+        public ProfessorDAO() 
+        { 
+        Conexao = new SqlConnection(LinhaConexao);
+        }
+        public void Inserir(ProfessoresEntidades professor)
+        {
+            Conexao.Open();
+            string query = "insert into Professores (Nome, Apelido) values (@nome, @apelido) ";
+            SqlCommand comando = new SqlCommand(query, Conexao);
+            SqlParameter parametro1 = new SqlParameter("@nome",professor.Nome);
+            SqlParameter parametro2 = new SqlParameter("@apelido",professor.Apelido);
+            comando.Parameters.Add(parametro1);
+            comando.Parameters.Add(parametro2);
+            comando.ExecuteNonQuery();
+            Conexao.Close();
+        }
+    }
+}
