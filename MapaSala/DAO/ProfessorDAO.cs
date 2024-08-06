0a7c9c9455dcbf9ef5056b15462b02f2abd4c252
ZsScartz <raphaelramos2006@gmail.com>
2024-08-06 10:12:37 -0300

    Mais coisas

--- MapaSala/DAO/ProfessorDAO.cs
+++ MapaSala/DAO/ProfessorDAO.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace MapaSala.DAO
 {
@@ -28,5 +29,33 @@ namespace MapaSala.DAO
             comando.ExecuteNonQuery();
             Conexao.Close();
         }
+        public DataTable ObterProfessores()
+        {
+
+            DataTable dt = new DataTable();
+            Conexao.Open();
+            string query = "SELECT * from Professores";
+            SqlCommand comando = new SqlCommand(query, Conexao);
+
+            SqlDataReader Leitura = comando.ExecuteReader();
+            foreach (var atributos in typeof(ProfessoresEntidades).GetProperties())
+            {
+                dt.Columns.Add(atributos.Name);
+            }
+            if (Leitura.HasRows)
+            {
+                while (Leitura.Read())
+                {
+                    ProfessoresEntidades p = new ProfessoresEntidades();
+                    p.Id = Convert.ToInt32(Leitura[0]);
+                    p.Nome = Leitura[1].ToString();
+                    p.Apelido = Leitura[2].ToString();
+                    dt.Rows.Add(p.Linha());
+                }
+            }
+               
+            return dt;
+        }
+
     }
 }
